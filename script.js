2f66a67293645bf4d101a6628b93d85ba0a9cf25
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-15 20:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const h1 = document.createElement('h1');
+// console.log(h1);
+h1.id = 'sizeInfo';
+
+helper();
+document.body.appendChild(h1);
+
+window.addEventListener('resize', helper)
+
+function helper() {
+    const x = window.innerWidth;
+	const y = window.innerHeight;
+
+	const textNode = document.createTextNode(`Width: ${x} and Height: ${y}`);
+
+    h1.innerHTML = '';
+	h1.appendChild(textNode);
+}
